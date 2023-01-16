91eea8dd7984c9607ce1243f0d8e5fb8cc9c8521
Peter <74869040+pszemraj@users.noreply.github.com>
2023-01-16 13:16:24 +0100

    ✏️ fix API typo
    Signed-off-by: Peter <74869040+pszemraj@users.noreply.github.com>

--- rpunct/punctuate.py
+++ rpunct/punctuate.py
@@ -38,7 +38,7 @@ class RestorePuncts:
             "felflare/bert-restore-punctuation",
             labels=self.valid_labels,
             args={"silent": True, "max_seq_length": 512},
-            has_cuda=get_cuda_status(),
+            use_cuda=get_cuda_status(),
         )
 
     def punctuate(self, text: str, lang: str = ""):
