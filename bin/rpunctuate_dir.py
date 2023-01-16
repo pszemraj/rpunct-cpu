9408959c8e6ae8e50800d467a7d96cb76f0c3356
Peter <74869040+pszemraj@users.noreply.github.com>
2023-01-16 15:11:32 +0100

    ✨ infer model type
    Signed-off-by: Peter <74869040+pszemraj@users.noreply.github.com>

--- bin/rpunctuate_dir.py
+++ bin/rpunctuate_dir.py
@@ -62,6 +62,26 @@ def create_archive(input_dir, archive_name="repunctuated-data") -> Path:
     return archive_path
 
 
+def infer_model_type(model_name_or_path: str or Path) -> str:
+    """
+    infer_model_type - infer the simpletransformers model type from the model name or path
+        https://simpletransformers.ai/docs/ner-specifics/
+
+    :param str model_name_or_path: the name or path of the model
+    :return str: the model type
+    """
+    model_name_or_path = Path(model_name_or_path).name
+    model_type = re.search(
+        r"(bert|xlnet|roberta|mobilebert|deberta-v2|albert)",
+        model_name_or_path,
+        re.IGNORECASE,
+    )
+    if model_type:
+        return model_type.group(1)
+    else:
+        raise ValueError(f"model type not recognized: {model_name_or_path}")
+
+
 def correct_text(rpunct, input_dir: str or Path, lowercase_inputs=True) -> Path:
     """
     correct_text is a function that corrects the punctuation in a text file.
